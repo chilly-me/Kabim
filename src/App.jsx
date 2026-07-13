a236f80768e3cafe1d1a0dd4de5b345ca27aacaf
chilly-me <calebchebii9@gmail.com>
2026-07-13 21:27:44 +0300

    Changed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Home.jsx";
-import Ship from "./ShipModel.jsx";
-import Courtyard from "./Courtyard.jsx";
 import Cottage from "./Cottage.jsx";
-import Box from "./Extruded_boxModel.jsx";
 
 const App = () => {
   return (
     <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/ship" element={<Ship />} />
-          <Route path="/courtyard" element={<Courtyard />} />
           <Route path="/cottage" element={<Cottage />} />
-          <Route path="/box" element={<Box />} />
         </Routes>
     </Router>
   );
